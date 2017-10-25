571810e09321d8141188884234f927e6006a604c
Krzysi3k <global.takeover@o2.pl>
2017-10-25 23:31:00 +0200

    .

---
+++ Benchmark/Benchmark/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benchmark
+{
+    class Program
+    {
+        public static void reverseArray(int[] array)
+        {
+            for (int i = 0; i < array.Length >> 1; i++)
+            {
+                int lastIdx = array.Length - i - 1;
+                int lastValue = array[lastIdx];
+                array[lastIdx] = array[i];
+                array[i] = lastValue;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.Error.WriteLine("Usage: mono Program <size> <iterations>");
+                Environment.Exit(1);
+            }
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            int[] myList = new int[Convert.ToInt32(args[0])];
+            for (int i = 0; i < myList.Length; i++)
+            {
+                myList[i] = i;
+            }
+
+            int count = Convert.ToInt32(args[1]);
+            for (int i = 0; i < count; i++)
+            {
+                reverseArray(myList);
+            }
+            timer.Stop();
+            TimeSpan elapsed = timer.Elapsed;
+            Console.WriteLine("finisehd! Total time in seconds: " + elapsed.TotalSeconds);
+            Console.ReadLine();
+        }
+    }
+}
